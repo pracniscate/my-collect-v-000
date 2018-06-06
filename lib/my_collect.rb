c1cca181755b7ecc6c17cf1e064ad9826769095e
Lorena <lorena.giddens@outlook.com>
2018-06-06 02:19:43 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,9 +1,12 @@
 def my_collect(collection)
   i = 0
+  # set a var collection to an empty array
   new_collection = []
   while i < collection.length
+    # push the return value into the collection array
     new_collection << yield(collection[i])
     i += 1
   end
+  # return the new collection at the end of the method
   new_collection
 end
